20ae9073947a0a1585fe3eab85a4b087dc793c02
vilhos <90275674+vilhos@users.noreply.github.com>
2021-11-14 22:21:01 +0400

    Update Program.cs

--- ShopAndShip/Program.cs
+++ ShopAndShip/Program.cs
@@ -10,35 +10,36 @@ namespace ShopAndShip
     {
         static void Main(string[] args)
         {
-            OnlineStore store = new OnlineStore();              
-            store.products.Add(new Product(1.5f, "box", 15f));
+            OnlineStore store = new OnlineStore();
+            store.products.Add(new Product(1.5f, "box", 15f));    // adding products to store
             store.products.Add(new Product(105f, "dox", 25f));
             store.products.Add(new Product(80.5f, "fox", 5f));
+            store.products.Add(new Product(80.5f, "mox", 7f));
 
-            store.cartProducts.Add(store.GetProduct("box"));
+            store.cartProducts.Add(store.GetProduct("box"));     // adding products to cart
             store.cartProducts.Add(store.GetProduct("dox"));
             List<Product> orderedProducts1 = store.Order();
-            store.Shipping();
-            store.CashDeleting();
+            store.Shipping();                                    // dispatch of products 
+            store.CashDeleting();                                // cleaning cache
 
             UsaWarehouse usa = new UsaWarehouse();
-            UsaWarehouse.collection.AddRange(orderedProducts1);
-            usa.Shipping();
-            usa.CashDeleting();
-            float MyCost = usa.Cost(orderedProducts1[0].Weight);
+            UsaWarehouse.collection.AddRange(orderedProducts1);  // Acceptance of delivered products
+            usa.Shipping();                                      
+            usa.CashDeleting();                                  
+            float MyCost = usa.Cost(orderedProducts1[0].Weight); // cost for one item delivery
             Console.WriteLine(MyCost);
 
             store.cartProducts.Add(store.GetProduct("fox"));
-            store.cartProducts.Add(store.GetProduct("fox"));
+            store.cartProducts.Add(store.GetProduct("mox"));
             List<Product> orderedProducts2 = store.Order();
+            store.Shipping();
+            store.CashDeleting();
 
             IShipService russia = new RussiaWarehouse();
             RussiaWarehouse.collection.AddRange(orderedProducts2);
             russia.Shipping();
 
-
             Console.Read();
         }
     }
 }
-
